d77cc74ba903d9993e118bde4078354690458fbb
tungdzai <87531719+tungdzai@users.noreply.github.com>
2024-07-31 03:53:38 +0700

    Update index.js

--- index.js
+++ index.js
@@ -216,7 +216,7 @@ async function processPhoneNumber(phone) {
 }
 async function checkVoucher(){
     try {
-        const listPhone = await readTokensFromFile('test.txt');
+        const listPhone = await readTokensFromFile('data.txt');
         await Promise.all(listPhone.map(phone => processPhoneNumber(`0${phone}`)));
     } catch (error) {
         console.error('Error reading phone numbers from file:', error.message);
